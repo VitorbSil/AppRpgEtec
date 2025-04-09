05335f0b3ec2fb64fcdfb331b4658e40fb857ebc
Vitor Barbosa <Vitorbarbosa062@gmail.com>
2025-04-08 22:28:14 -0300

    Adição do CRUD de Armas. INCOMPLETO

---
+++ Views/Armas/ListagemView.xaml.cs
@@ -0,0 +1,16 @@
+using AppRpgEtec.ViewModels.Armas;
+
+namespace AppRpgEtec.Views.Armas;
+
+public partial class ListagemView : ContentPage
+{
+    ListagemArmaViewModel viewModel;
+    public ListagemView()
+    {
+        InitializeComponent();
+
+        viewModel = new ListagemArmaViewModel();
+        BindingContext = viewModel;
+        Title = "Armas - App Rpg Etec";
+    }
+}
