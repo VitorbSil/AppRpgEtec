21e68fbd4330989b48968f47e8146d2dc29d9974
Vitor Barbosa <Vitorbarbosa062@gmail.com>
2025-04-15 22:24:30 -0300

    Otimização da página inicial. Adicionados ícones e novas funções na tela inicial

---
+++ AppRpgEtec/Services/Usuarios/UsuarioService.cs
@@ -0,0 +1,36 @@
+﻿using AppRpgEtec.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppRpgEtec.Services.Usuarios
+{
+    public class UsuarioService
+    {
+        private readonly Request _request;
+        private const string _apiUrlBase = "http://luizsouza.somee.com/RpgApi/Usuarios";
+
+        public UsuarioService()
+        {
+            _request = new Request();
+        }
+
+        public async Task<Usuario> PostRegistrarUsuarioAsync(Usuario u)
+        {
+            string urlComplementar = "/Registrar";
+            u.Id = await _request.PostReturnIntAsync(_apiUrlBase + urlComplementar, u, string.Empty);
+
+            return u;
+        }
+
+        public async Task<Usuario> PostAutenticarUsuarioAsync(Usuario u)
+        {
+            string urlComplementar = "/Autenticar";
+            u = await _request.PostAsync(_apiUrlBase + urlComplementar, u, string.Empty);
+
+            return u;
+        }
+    }
+}
