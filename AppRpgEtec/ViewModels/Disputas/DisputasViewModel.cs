0bb4ef458dfb31c2812ace1d1a89ef98fbbc2ffe
Vitor Barbosa <Vitorbarbosa062@gmail.com>
2025-05-27 22:22:56 -0300

    feat: Adição da busca por personagens nas Disputas, e tela para tal.

---
+++ AppRpgEtec/ViewModels/Disputas/DisputasViewModel.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using AppRpgEtec.Models;
+using AppRpgEtec.Services.Personagens;
+
+namespace AppRpgEtec.ViewModels.Disputas
+{
+    public class DisputasViewModel : BaseViewModel
+    {
+        public string DescricaoPersonagemAtacante
+        {
+            get => Atacante.Nome;
+        }
+
+        public string DescricaoPersonagemOponente
+        {
+            get => Oponente.Nome;
+        }
+
+        private Personagem personagemSelecionado;
+        public Personagem PersonagemSelecionado
+        {
+            set
+            {
+                if(value != null)
+                {
+                    personagemSelecionado = value;
+                    SelecionarPersonagem(personagemSelecionado);
+                    OnPropertyChanged();
+                    PersonagensEncontrados.Clear();
+                }
+            }
+        }
+        
+        private string textoBuscaDigitado = string.Empty;
+        public string TextoBuscaDigitado
+        {
+            get { return textoBuscaDigitado; }
+            set
+            {
+                if ((value != null && !string.IsNullOrEmpty(value) && value.Length > 0))
+                {
+                    textoBuscaDigitado = value;
+                    _ = PesquisarPersonagens(textoBuscaDigitado);
+                }
+                else
+                {
+                    PersonagensEncontrados.Clear();
+                }
+            }
+        }
+
+
+        private PersonagemService pService;
+
+        public ObservableCollection<Personagem> PersonagensEncontrados { get; set; }
+        public Personagem Atacante;
+        public Personagem Oponente;
+
+        public DisputasViewModel()
+        {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            pService = new PersonagemService(token);
+
+            Atacante = new Personagem();
+            Oponente = new Personagem();
+
+            PersonagensEncontrados = new ObservableCollection<Personagem>();
+
+            PesquisarPersonagemCommand = 
+                new Command<string>(async (string pesquisa) => { await PesquisarPersonagens(pesquisa); });
+        }
+
+        public ICommand PesquisarPersonagemCommand { get; set; }
+
+        public async Task PesquisarPersonagens(string textoPesquisaPersonagem)
+        {
+            try
+            {
+                PersonagensEncontrados = await pService.GetByNomeAproximadoAsync(textoPesquisaPersonagem);
+                OnPropertyChanged(nameof(PersonagensEncontrados));
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+
+        public async void SelecionarPersonagem(Personagem p)
+        {
+            try
+            {
+                string tipoCombate = await Application.Current.MainPage
+                    .DisplayActionSheet("Atacante ou Oponente?", "Cancelar", "", "Atacante", "Oponente");
+
+                if(tipoCombate == "Atacante")
+                {
+                    Atacante = p;
+                    OnPropertyChanged(nameof(DescricaoPersonagemAtacante));
+                }
+                else if (tipoCombate == "Oponente")
+                {
+                    Oponente = p;
+                    OnPropertyChanged(nameof(DescricaoPersonagemOponente));
+                }
+            }
+            catch(Exception ex)
+            {
+                await Application.Current.MainPage
+                    .DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+
+
+
+
+
+    }
+}
